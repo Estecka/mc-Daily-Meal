431011194ef4101c7ee644cc6ec15da4fa80f4b8
Estecka <estecka@galacsys.com>
2024-07-30 22:14:48 +0200

    Fixed compilation errors

--- src/main/java/tk/estecka/dailymeal/FoodRebalance.java
+++ src/main/java/tk/estecka/dailymeal/FoodRebalance.java
@@ -1,11 +1,13 @@
 package tk.estecka.dailymeal;
 
-import net.minecraft.item.FoodComponent;
+import net.minecraft.component.type.FoodComponent;
 import tk.estecka.dailymeal.mixin.IFoodComponentAccessor;
-import static net.minecraft.item.FoodComponents.*;
+import static net.minecraft.component.type.FoodComponents.*;
 
-public class FoodRebalance {
-	static public final FoodComponent CAKE_SLICE = new FoodComponent.Builder().hunger(2).saturationModifier(0.1f).build();
+
+public class FoodRebalance
+{
+	static public final FoodComponent CAKE_SLICE = new FoodComponent.Builder().nutrition(2).saturationModifier(0.1f).build();
 
 	static public void Register(){
 		setQuality(0, POISONOUS_POTATO, ROTTEN_FLESH, SPIDER_EYE);
@@ -19,16 +21,16 @@ public class FoodRebalance {
 
 	static private void	setHunger(int hunger, FoodComponent... foods){
 		for (FoodComponent f : foods)
-			((IFoodComponentAccessor)f).setHunger(hunger);
+			((IFoodComponentAccessor)(Object)f).setNutrition(hunger);
 	}
 
 	static private void	setQuality(float quality, FoodComponent... foods){
 		for (FoodComponent f : foods)
-			((IFoodComponentAccessor)f).setSaturationModifier(quality);
+			((IFoodComponentAccessor)(Object)f).setSaturation(quality);
 	}
 
 	static private void	setEdible(boolean setEdible, FoodComponent... foods){
 		for (FoodComponent f : foods)
-			((IFoodComponentAccessor)f).setAlwaysEdible(setEdible);;
+			((IFoodComponentAccessor)(Object)f).setCanAlwaysEat(setEdible);;
 	}
 }
--- src/main/java/tk/estecka/dailymeal/mixin/IFoodComponentAccessor.java
+++ src/main/java/tk/estecka/dailymeal/mixin/IFoodComponentAccessor.java
@@ -3,13 +3,13 @@ package tk.estecka.dailymeal.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import net.minecraft.component.type.FoodComponent;
 
-import net.minecraft.item.FoodComponent;
 
 @Mixin(FoodComponent.class)
-public interface IFoodComponentAccessor 
+public interface IFoodComponentAccessor
 {
-	@Mutable @Accessor void setHunger(int value);
-	@Mutable @Accessor void setSaturationModifier(float value);
-	@Mutable @Accessor void setAlwaysEdible(boolean value);
+	@Mutable @Accessor void setNutrition(int value);
+	@Mutable @Accessor void setSaturation(float value);
+	@Mutable @Accessor void setCanAlwaysEat(boolean value);
 }
