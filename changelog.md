6488309b5390634aedf67f1fc4b10a97c7dc8670
Estecka <estecka@galacsys.com>
2024-07-31 15:59:13 +0200

    Increased default duration

--- changelog.md
+++ changelog.md
@@ -1,5 +1,6 @@
 # v1
+### 1.0.0
 Initial release
-
-# v2
+### 1.0.1
 - Beetroot stew may now be crafted using red mushrooms.
+- The default duration of the bonuses was increased to 3 days. This align with the apparition of phantoms
--- src/main/java/tk/estecka/dailymeal/MealRules.java
+++ src/main/java/tk/estecka/dailymeal/MealRules.java
@@ -10,7 +10,7 @@ import static net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory.createIntRule;
 public class MealRules
 {
 	static public final Key<BooleanRule> NATURAL_REGENERATION = GameRules.NATURAL_REGENERATION;
-	static public final	Key<DoubleRule>  DAILY_BONUS_DURATION = Register("dailyBonusDuration", createDoubleRule(1.0, 0.0));
+	static public final	Key<DoubleRule>  DAILY_BONUS_DURATION = Register("dailyBonusDuration", createDoubleRule(3.0, 0.0));
 	static public final	Key<IntRule>     SLEEP_COST = Register("sleepFoodCost", createIntRule(12, 0, 20));
 
 	static public void	Register(){
